16905dc4508de8aa2eb18959789cadd520454695
Capucine Plantecoste <capucine.plantecoste@gmail.com>
2021-10-03 16:34:49 +0200

    debut tp

--- js/API_Weather.js
+++ js/API_Weather.js
@@ -7,11 +7,10 @@ const API_URL = "https://api.openweathermap.org/data/2.5/weather";
 // Base source icon
 const API_URL_ICON = "http://openweathermap.org/img/wn/";
 
-
-class API_WEATHER{
-  constructor(city){
-    // Si la ville n'est pas définit alors la ville par défault est Paris
-    if(city === undefined){
+class API_WEATHER {
+  constructor(city) {
+    // Si la ville n'est pas définie alors la ville par défault est Paris
+    if (city === undefined) {
       city = "paris";
     }
     this.city = city;
@@ -19,14 +18,14 @@ class API_WEATHER{
 
   // Faire la requete à l'API openweathermap
   // Retourne une promise
-  fetchTodayForecast(){
+  fetchTodayForecast() {
     return axios
-    .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
-      crossdomain: true
-    })
+      .get(`${API_URL}?q=${this.city}&units=metric&appid=${API_KEY}`, {
+        crossdomain: true
+      })
   }
   // Retourne l'element HTML de l'icon symbolisant la méteo.
-  getHTMLElementFromIcon(icon){
+  getHTMLElementFromIcon(icon) {
     return `<img src=${API_URL_ICON}${icon}@2x.png class="weather-icon"/>`
   }
 }
--- js/script.js
+++ js/script.js
@@ -7,11 +7,11 @@ function start() {
 
   apiWeather
     .fetchTodayForecast()
-    .then(function(response) {
+    .then(function (response) {
       // Récupère la donnée d'une API
       const data = response.data;
 
-      // On récupère l'information principal
+      // On récupère l'information principale
       const main = data.weather[0].main;
       const description = data.weather[0].description;
       const temp = data.main.temp;
@@ -22,9 +22,9 @@ function start() {
       document.getElementById('today-forecast-more-info').innerHTML = description;
       document.getElementById('icon-weather-container').innerHTML = icon;
       document.getElementById('today-forecast-temp').innerHTML = `${temp}°C`;
-      
+
     })
-    .catch(function(error) {
+    .catch(function (error) {
       // Affiche une erreur
       console.error(error);
     });
